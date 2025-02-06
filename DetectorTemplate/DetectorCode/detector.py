022bb0dea574f95fe57757177d1ff43abffe88db
Roberta Du <robertadu@Robertas-Air.hitronhub.home>
2025-02-05 22:20:59 -0500

    updated bot detector logic to account for bot features

--- DetectorTemplate/DetectorCode/detector.py
+++ DetectorTemplate/DetectorCode/detector.py
@@ -6,18 +6,38 @@ class Detector(ADetector):
         # todo logic
         # Example:
         marked_account = []
-        
 
         for user in session_data.users:
 
-            bot_status = False
+            bot_features = 0 
 
-            username = user["username"]
 
-            if username == "I_am_a_bot":
-                bot_status = True
+            # Bot is in username, you're cut 
+            if "bot" in user['user_id'].lower() | "bot" in user['username'].lower() | "bot" in user['name'].lower():
+                bot_features += 1 
+
+            # Numeric heavy username 
+            if sum(c.isdigit() for c in user["username"]) > 5:
+                bot_features += 1
+
+            # Empty profile description 
+            if user["description"].strip() == "":
+                bot_features += 1
+
+            # High tweet count 
+            if user['tweet_count'] > 90:
+                bot_symptoms += 1 
+            
+            # Extreme z-score 
+            if user['z_score'] > 3:
+                bot_features += 1
+            
+            # If a user has more than half, then is a bot 
+
+            bot_status = bot_features >= 3
+
     
             marked_account.append(DetectionMark(user_id=user['id'], confidence=50, bot=bot_status))
 
-        return marked_account
+            return marked_account
     
