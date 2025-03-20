79dae0b28f83324c0ab8ac24a3b8355fd5175300
Roberta Du <robertadu@Robertas-Air.hitronhub.home>
2025-03-20 00:44:18 -0400

    implemented rudimentary logistic regression model

--- DetectorTemplate/DetectorCode/detector.py
+++ DetectorTemplate/DetectorCode/detector.py
@@ -1,22 +1,42 @@
 from abc_classes import ADetector
 from teams_classes import DetectionMark
 import json 
-import datetime 
-import math 
-import statistics
-import re
+import pickle 
+import pandas as pd 
+import numpy as np 
+
 
 class Detector(ADetector):
-
 
     def detect_bot(self, session_data):
         marked_accounts = []
 
-        for user in session_data.users:
-            bot_status = True 
-            confidence = 50
+        # Load the model
+        with open("logistic_regression_model.pkl", "rb") as model_file:
+            model = pickle.load(model_file)
 
-            marked_accounts.append(DetectionMark(user_id=user['id'], confidence=confidence, bot=bot_status))
+        # Load the scaler
+        with open("scaler.pkl", "rb") as scaler_file:
+            scaler = pickle.load(scaler_file)
+
+        # Transforming the data into a pandas dataframe 
+        session_df = pd.DataFrame(session_data.users)
+        # print(session_df)
+
+        # Scale the new data using the loaded scaler
+        session_df.rename(columns={"id": "user_id"}, inplace=True)
+        X_session = session_df[['tweet_count', 'z_score']]  
+        X_session_scaled = scaler.transform(X_session)
+
+        # Get probability of bot 
+        bot_probs = model.predict_proba(X_session_scaled)[:, 1]
+
+        # Get binary predictions 
+        bot_labels = model.predict(X_session_scaled)
+
+        # Creating the annotated dataset 
+        for user_id, bot, prob in zip(session_df['user_id'], bot_labels, bot_probs):
+            marked_accounts.append(DetectionMark(user_id=user_id, confidence=int(prob * 100), bot=bool(bot)))
 
         return marked_accounts
 
--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM python:3
 RUN pip install requests
 RUN pip install pydantic
 RUN pip install numpy 
+RUN pip install pandas 
 RUN pip install scikit-learn 
+RUN pip install pickle-mixin
 
 #Important so we will have access to the run.sh file 
 COPY . . 
